be371e0b9a784bb7111946944ec666d4738d6fcf
Jonathan Malave <95556326+jonathanmal1119@users.noreply.github.com>
2026-02-16 18:31:43 -0500

    Change script path from server.js to index.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "LetUsCook-Backend",
-        script: "./src/server.js",
+        script: "./src/index.js",
         cron_restart: "0 */6 * * *",
         env: {
           NODE_ENV: "production"
