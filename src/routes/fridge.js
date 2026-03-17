5c664ed76e89732a7473d00f9bcd72339c40e476
rrojas1819 <rrojas181920@gmail.com>
2026-03-17 17:09:16 -0400

    StrictLimited added to the receipt

--- src/routes/fridge.js
+++ src/routes/fridge.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/auth.middleware');
-const { apiLimiter } = require('../middleware/rateLimiters');
+const { apiLimiter, strictLimiter } = require('../middleware/rateLimiters');
 const { upload } = require('../middleware/upload');
 const { addItemToFridge, uploadReceiptToFridge, confirmReceiptItems, updateItemInFridge, getUserFridge,removeItemFromFridge } = require('../controllers/fridgeController');
 
 
-router.post('/receipt', verifyToken, apiLimiter, upload.single('receipt'), uploadReceiptToFridge);
+router.post('/receipt', verifyToken, strictLimiter, upload.single('receipt'), uploadReceiptToFridge);
 router.post('/receipt/confirm', verifyToken, apiLimiter, confirmReceiptItems);
 router.post('/add-item', apiLimiter, verifyToken, addItemToFridge);
 router.delete('/remove-item', apiLimiter, verifyToken, removeItemFromFridge);
