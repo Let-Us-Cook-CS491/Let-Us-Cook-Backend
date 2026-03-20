3049dddccea0867e884e93f27a830c80ec7c47eb
Jonathan Malave <jmaxmalave@aol.com>
2026-03-20 17:13:06 -0400

    Endpoint to set Health goals complete

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
+const db = require('../config/databaseConnection');
 const UserPreference = require('../schemes/userPreferences');
 const { connectMongo } = require('../config/databaseConnection');
+const { isValidGoal, isValidActivityLevel } = require('../middleware/helperFunctions');
+
 
 exports.setUserPreference = async (req, res) => {
     try {
@@ -73,3 +76,74 @@ exports.setUserPreference = async (req, res) => {
         });
     }
 };     
+
+exports.setHealthGoals = async (req, res) => {
+    const { goal, activity_level, calorie_target } = req.body;
+    let normalized_calorie_target = calorie_target || null
+
+    const rawUserId = req.user?.user_id;
+    const user_id = Number(rawUserId);
+    if (!Number.isInteger(user_id)) {
+        return res.status(401).json({
+            status: "ERROR",
+            message: "Unauthorized User",
+        });
+    }
+
+    if (!goal || !activity_level) {
+        return res.status(400).json({
+            status: "ERROR",
+            message: "Goal and Activity Level fields are required",
+        });
+    }
+
+    if (!isValidGoal(goal)) {
+        return res.status(400).json({
+            status: "ERROR",
+            message: "Invalid goal value",
+        });
+    }
+
+    if (!isValidActivityLevel(activity_level)) {
+        return res.status(400).json({
+            status: "ERROR",
+            message: "Invalid activity level value",
+        });
+    }
+
+    try {
+        connection = await db.getConnection();
+
+        const updateUserHealthGoalQuery = 
+        `INSERT INTO health_goals (user_id, goal, activity_level, calorie_target)
+        VALUES (?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE 
+        goal = VALUES(goal),
+        activity_level = VALUES(activity_level),
+        calorie_target = VALUES(calorie_target);`;
+
+        const [updateUserHealthGoalResult] = await connection.execute(updateUserHealthGoalQuery, [user_id, goal, activity_level, normalized_calorie_target]);
+
+        if (updateUserHealthGoalResult.length === 0) {
+            return res.status(401).json({
+                status: "ERROR",
+                message: "Failed to insert into Table",
+            });
+        }
+
+        await connection.commit();
+
+        res.status(200).json({
+            status: "OK",
+            message: "User health goals set successfully",
+        });
+    } catch (error) {
+        res.status(401).json({
+            status: "ERROR",
+            message: "Failed to set user health goals",
+            error: error.message,
+        });
+    } finally {
+        if (connection) await connection.release();
+    }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middleware/auth.middleware');
 const { apiLimiter, strictLimiter } = require('../middleware/rateLimiters');
-const { setUserPreference } = require('../controllers/userController');
+const { setUserPreference, setHealthGoals } = require('../controllers/userController');
 
 
 router.post('/set-preferences', apiLimiter, verifyToken, setUserPreference);
+router.post('/set-health-goals', apiLimiter, verifyToken, setHealthGoals);
 
 
 module.exports = router;
