eb3098d20b826445148aff9982881ac9ecfe54fe
Jonathan Malave <jmaxmalave@aol.com>
2026-04-19 14:46:21 -0400

    new fridge is created on user sign up

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,7 +46,7 @@ exports.signup = async (req, res) => {
     }
 
     // Validate password length
-    if (password.length < 6) {
+    if (password.length < 8) {
         return res.status(400).json({
             status: "ERROR",
             message: "Password must be at least 6 characters",
@@ -83,9 +83,22 @@ exports.signup = async (req, res) => {
         const saltRounds = process.env.SALT_ROUNDS || 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
+
+        // Create new Fridge
+        const insertFridgeQuerry = `INSERT INTO fridge () VALUES ()`;
+        const [insertFridgeEventResult] = await connection.execute(insertFridgeQuerry);
+
+        if (insertFridgeEventResult.affectedRows === 0) {
+            await connection.rollback();
+            return res.status(500).json({
+                status: "ERROR",
+                message: "Failed to insert login event",
+            });
+        }
+
         // Insert new user
-        const insertUserQuery = `INSERT INTO users (full_name, email, phone, gender, time_zone) VALUES (?, ?, ?, ?, ?)`;
-        const [insertUserResult] = await connection.execute(insertUserQuery, [full_name, email, phoneNumber, gender, time_zone]);
+        const insertUserQuery = `INSERT INTO users (full_name, email, phone, gender, time_zone, fridge_id) VALUES (?, ?, ?, ?, ?, ?)`;
+        const [insertUserResult] = await connection.execute(insertUserQuery, [full_name, email, phoneNumber, gender, time_zone, insertFridgeEventResult.insertId]);
 
         if (insertUserResult.affectedRows === 0) {
             await connection.rollback();
